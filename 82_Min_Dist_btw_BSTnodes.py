10dd4e9fc9c3ea131dd9301da9aee7c0d79958bd
Data-analyst-Jay <jaygehlot661@gmail.com>
2025-06-13 09:08:17 +0530

    Solved the problem to find the minimum difference between the node values of a BST

---
+++ 82_Min_Dist_btw_BSTnodes.py
@@ -0,0 +1,22 @@
+#TC = O(n)
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    prev = None
+    def minDiffInBST(self, root):
+        ans = float('inf')
+        if not root:
+            return float('inf')
+        if root.left:
+            leftmin = self.minDiffInBST(root.left)
+            ans = min(ans, leftmin)
+        if self.prev:
+            ans = min(ans, root.val - self.prev)
+        self.prev = root.val
+        if root.right:
+            rightmin = self.minDiffInBST(root.right)
+            ans = min(ans, rightmin)
+        return ans
