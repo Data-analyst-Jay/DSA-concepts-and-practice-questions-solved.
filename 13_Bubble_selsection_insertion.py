589988048af6a5f622806262a1321c8fee96ab5a
Data Analyst Jay <jaygehlot661@gmail.com>
2025-07-26 09:31:44 +0530

    Resolved selection and insertion sort

--- 13_Bubble_selsection_insertion.py
+++ 13_Bubble_selsection_insertion.py
@@ -13,4 +13,27 @@ def bubble(arr):
     return arr
 
 a = [4,3,1,5,2]
-print(bubble(a))
+# print(bubble(a))
+
+def selection(arr):
+    for i in range(len(arr)):
+        mini = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[mini]:
+                mini = j
+        arr[i], arr[mini] = arr[mini], arr[i]
+    return arr
+
+# print(selection(a))
+
+def insertion(arr):
+    for i in range(1, len(arr)):
+        s = arr[i]
+        j = i-1
+        while j >= 0 and s < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = s
+    return arr
+
+print(insertion(a))
