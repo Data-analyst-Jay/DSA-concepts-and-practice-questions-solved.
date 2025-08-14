17b6b3d5f1f66bd04caf4cde023d41b2591a9533
Data Analyst Jay <jaygehlot661@gmail.com>
2025-08-14 09:04:23 +0530

    Added my way of program

--- 17_Permutation_in_string.py
+++ 17_Permutation_in_string.py
@@ -19,3 +19,21 @@ def permu_str(str1,str2):
 
 ans = permu_str('a','ab')
 print(ans)
+
+# MY WAY
+
+def permutations(s1, s2):
+    if len(s1) > len(s2):
+        return False
+    n = len(s1)
+    f1 = [0]*26
+    for i in range(len(s1)):
+        f1[ord(s1[i])-ord('a')] += 1
+    
+    for i in range(len(s2)-n+1):
+        f2 = [0]*26
+        for j in range(i, i+n):
+            f2[ord(s2[j])-ord('a')] += 1
+        if f1 == f2:
+            return True
+    return False
